79aa93a1c83b418bb7b835b4da776942a446a0ad
lizhongyangyang <1007845131@qq.com>
2016-08-30 13:17:49 +0800

    UI布局

---
+++ app/src/main/java/com/aichifan/app4myqa/EventManageActivity.java
@@ -0,0 +1,13 @@
+package com.aichifan.app4myqa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventManageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_manage);
+    }
+}
---
+++ app/src/main/java/com/aichifan/app4myqa/WMSClosedActivity.java
@@ -0,0 +1,13 @@
+package com.aichifan.app4myqa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WMSClosedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wmsclosed);
+    }
+}
