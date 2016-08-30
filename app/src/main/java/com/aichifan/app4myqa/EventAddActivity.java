a86dde03e8637f009f8fa549f50caa214344b93e
cuiyuanhang <374687636@qq.com>
2016-08-30 15:18:04 +0800

    初始化

---
+++ app/src/main/java/com/aichifan/app4myqa/EventAddActivity.java
@@ -0,0 +1,16 @@
+package com.aichifan.app4myqa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mic on 2016/8/30.
+ */
+public class EventAddActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_add);
+    }
+}
