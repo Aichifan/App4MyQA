b403ec3d907c841f53f9922423eb70eb520a52b9
suli520 <455917428@qq.com>
2016-08-31 19:42:10 +0800

    初次提交

---
+++ app/src/main/java/com/aichifan/app4myqa/vo/TableModel.java
@@ -0,0 +1,34 @@
+package com.aichifan.app4myqa.vo;
+
+public class TableModel {
+
+    private Integer draw;
+
+    private Integer start;
+
+    private Integer length;
+
+    public Integer getDraw() {
+        return draw;
+    }
+
+    public void setDraw(Integer draw) {
+        this.draw = draw;
+    }
+
+    public Integer getStart() {
+        return start;
+    }
+
+    public void setStart(Integer start) {
+        this.start = start;
+    }
+
+    public Integer getLength() {
+        return length;
+    }
+
+    public void setLength(Integer length) {
+        this.length = length;
+    }
+}
